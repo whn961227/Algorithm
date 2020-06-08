069ee998cda758b02f9cba69ea7f64bc17ff039b
whn961227 <479849579@qq.com>
2020-06-08 21:46:42 +0800

    默认

---
+++ src/main/java/com/whn/heap/FindKthLargest.java
@@ -0,0 +1,25 @@
+package com.whn.heap;
+
+import java.util.PriorityQueue;
+
+/**
+ * 在未排序的数组中找到第 k 个最大的元素。请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
+ * 输入: [3,2,1,5,6,4] 和 k = 2
+ * 输出: 5
+ */
+public class FindKthLargest {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> heap = new PriorityQueue<Integer>((n1, n2) -> n1 - n2);
+        for (int num : nums) {
+            heap.add(num);
+            if (heap.size() > k) {
+                heap.poll();
+            }
+        }
+        return heap.poll();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new FindKthLargest().findKthLargest(new int[]{3, 2, 1, 5, 6, 4}, 2));
+    }
+}
